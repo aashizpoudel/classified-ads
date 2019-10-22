fdeb318b5ca959c6f8beb3322cdd065c23136319
Aashiz Poudel <mr.aashiz@gmail.com>
2019-10-22 13:51:14 +0545

    before version upgrade

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         // Commands\Inspire::class,
-        Commands\ResetDatabase::class,
+        //Commands\ResetDatabase::class,
     ];
 
     /**
---
+++ database/seeds/OptionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OptionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+         //Insert country first
+         $option_sql = file_get_contents(__DIR__ . '/dumps/options.sql');
+         // split the statements, so DB::statement can execute them.
+         $option_statements = array_filter(array_map('trim', explode(';', $option_sql)));
+         foreach ($option_statements as $stmt) {
+             DB::statement($stmt);
+         }
+    }
+}
